2466e21afb573a505659071ef1b4afdc664cc180
Jack Geissinger <jackg0@users.noreply.github.com>
2019-10-08 08:59:24 -0400

    training models on cascades (#20)

--- src/train_position_inference.sh
+++ src/train_position_inference.sh
@@ -1,7 +1,8 @@
-python train-seq2seq.py -f /home/jack/research/ARL/xsens/position-inference/ \
+python train-seq2seq.py -f ./data/ \
+                        --model-file-path ./models/trained-model.pt \
                         --batch-size=32 \
                         --encoder-feature-size=12 \
                         --decoder-feature-size=3 \
-                        --num-epochs=5 \
+                        --num-epochs=20 \
                         --bidirectional \
                         --attention
