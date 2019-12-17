a658d6fd7fb86d15892b414be3165a560352f3c8
Jack Geissinger <jackg7@vt.edu>
2019-12-17 09:06:00 -0500

    adding logging file

---
+++ src/common/logging.py
@@ -0,0 +1,11 @@
+
+from __future__ import absolute_import
+
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
