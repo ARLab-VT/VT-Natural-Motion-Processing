e123634b044d87a8d9b887da80e0bc5d15fd84c1
Jack Geissinger <jackg7@vt.edu>
2020-08-07 13:14:09 -0400

    removing zero-velocity baseline because it is unused

--- src/zero_velocity.sh
+++
@@ -1,6 +0,0 @@
-
-python zero-velocity.py --data-path /groups/MotionPred/motion-prediction/full-set \
-                        --batch-size=16 \
-                        --seq-length=160 \
-                        --downsample=6 \
-                        --stride=80
